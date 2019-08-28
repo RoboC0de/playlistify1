7894d6fcb58d357cded067fd47c11599b216ad8f
Ahmadi <s.ahmadi@student.tue.nl>
2019-08-28 12:54:20 +0200

    Updates

---
+++ precache-manifest.52991bbbf35da50ed509ecf8bdd5d341.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2cbfa432264f1bcb589acaeb0d09d40",
+    "url": "/playlistify1/index.html"
+  },
+  {
+    "revision": "1472f4415fc44f0c9787",
+    "url": "/playlistify1/static/css/main.66b6fe2a.chunk.css"
+  },
+  {
+    "revision": "c87125fc3560f1d69814",
+    "url": "/playlistify1/static/js/2.7e474140.chunk.js"
+  },
+  {
+    "revision": "1472f4415fc44f0c9787",
+    "url": "/playlistify1/static/js/main.e68eeaf3.chunk.js"
+  },
+  {
+    "revision": "f7159895c0112a02a50c",
+    "url": "/playlistify1/static/js/runtime~main.f0b76658.js"
+  },
+  {
+    "revision": "37be7357a5f8583073df7e0aec787eaa",
+    "url": "/playlistify1/static/media/background_photo_desktop.37be7357.jpg"
+  }
+]);
